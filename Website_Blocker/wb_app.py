a8c673cdcf8a63118f82f7415d6305376070bbc6
Harman Bhatia <harmanbhatia101@gmail.com>
2019-09-10 20:46:38 +0530

    Website Blocked Application
    This python application worked as the background process in the PC and will block specified websites for particular time.

---
+++ Website_Blocker/wb_app.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime as dt
+
+website_list = ["www.facebook.com", "facebook.com"]
+redirects = "127.0.0.1"
+host_path = "C:\Windows\System32\drivers\etc\hosts"
+host_temp = "hosts"
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) <= dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 22, 10):
+        with open(host_temp, "r+") as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirects + " " + website+"\n")
+        print("Working hours")
+    else:
+        with open(host_temp, "r+") as file:
+            content = file.readlines()
+            file.seek(0)
+            for lines in content:
+                if not any(website in lines for website in website_list):
+                    file.write(lines)
+            file.truncate()
+        print("Chill mode On")
+    time.sleep(5)
